16eb2075ca2c5d1d38680a714b837942200e874d
Wellington Bezerra <wellingtonbezerra.30512@gmail.com>
2025-03-04 21:53:12 -0300

    feat: criando componente modal

---
+++ src/components/Modal.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../styles/Modal.css';
+
+function Modal({ isOpen, closeModal }) {
+  if (!isOpen) return null;  // Se o modal não estiver aberto, não renderiza nada
+
+  return (
+    <div className="modal-overlay">
+      <div className="modal">
+        <button className="close-button" onClick={closeModal}>X</button>
+        <div className="modal-content">
+          Modal
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
